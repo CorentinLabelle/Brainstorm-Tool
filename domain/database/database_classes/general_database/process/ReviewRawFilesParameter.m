c9fba6c22113274181f7756d022f4c23488836d1
CorentinLabelle <corentinlabelle2@gmail.com>
2023-02-12 13:34:31 -0500

    Add PreAssigningConvertFunction to ReviewRawFiles process

--- domain/database/database_classes/general_database/process/ReviewRawFilesParameter.m
+++ domain/database/database_classes/general_database/process/ReviewRawFilesParameter.m
@@ -1,21 +1,28 @@
 function parameters = ReviewRawFilesParameter()
     p1 = ParameterFactory.create('subject', 'cell', cell.empty());
     p2 = ParameterFactory.create('raw_files', 'cell', cell.empty());
+    p2 = p2.setPreAssigningConvertFunction(@convertRelativePathToAbsolute);
     p2 = p2.setValidityFunction(@AreFilesValid);
     p2 = p2.setConverterFunction(@RawFilesConverter);
     p3 = ParameterFactory.create('file_format', 'char', char.empty(), getPossibleFileFormat());
     parameters = {p1, p2, p3};
 end
     
-function AreFilesValid(value)
-    classOfCellContent = cellfun(@(x) class(x), value, 'UniformOutput', false);
+function AreFilesValid(files)
+    classOfCellContent = cellfun(@(x) class(x), files, 'UniformOutput', false);
     isCell = strcmpi(unique(classOfCellContent), 'cell');
     if isCell
-        for i = 1:length(value)
-            DoFilesExist(value{i});
+        for i = 1:length(files)
+            AreFilesValid(files{i});
         end
     else
-        DoFilesExist(value);
+        isFiles = cellfun(@(x) isfile(x), files);
+        invalidFiles = files(~isFiles);
+        if ~isempty(invalidFiles)
+            invalidFilesAsChars = char(strjoin(invalidFiles, '\n'));
+            errorMessage = ['The following files do not exist:' newline invalidFilesAsChars];
+            error(errorMessage);
+        end
     end
 end
 
@@ -34,12 +41,24 @@ function value = RawFilesConverter(value)
     end
 end
 
-function DoFilesExist(files)
-    isFiles = cellfun(@(x) isfile(x), files);
-    invalidFiles = files(~isFiles);
-    if ~isempty(invalidFiles)
-        invalidFilesAsChars = char(strjoin(invalidFiles, '\n'));
-        errorMessage = ['The following files do not exist:' newline invalidFilesAsChars];
-        error(errorMessage);
+function files = convertRelativePathToAbsolute(files)
+    classOfCellContent = cellfun(@(x) class(x), files, 'UniformOutput', false);
+    isCell = strcmpi(unique(classOfCellContent), 'cell');
+    if isCell
+        for i = 1:length(files)
+            files{i} = convertRelativePathToAbsolute(files{i});
+        end
+    else
+        bool = cellfun(@(x) startsWith(x, '.'), files);
+        if ~any(bool)
+            return
+        end
+        config = Configuration();
+        config = config.loadConfiguration();
+        for j = 1:length(bool)
+            if bool(j)
+                files{j} = fullfile(config.getDataPath(), files{j});
+            end
+        end
     end
 end
