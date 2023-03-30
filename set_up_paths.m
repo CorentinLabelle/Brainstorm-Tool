ebedc2d0d1a3a14b546a99d0b14bab439703cb61
CorentinLabelle <corentinlabelle2@gmail.com>
2023-03-30 13:06:27 -0400

    update set_up_paths

--- set_up_paths.m
+++ set_up_paths.m
@@ -1,12 +1,32 @@
-function set_up_paths()    
-    filePath = searchFolderForFile(pwd, 'PathsAdder.m');
-    addpath(filePath);
-    PathsAdder.addPaths();    
+function set_up_paths()
+    current_file_path = mfilename('fullpath');
+    current_file_dir = fileparts(current_file_path);
+    addpath(genpath(current_file_dir));
+    add_brainstorm3_path();
 end
 
-function filePath = searchFolderForFile(folder, filename)
-    fileList = dir(fullfile(folder, '**/*.*'));
-    fileList = fileList(~[fileList.isdir]);
-    file = fileList(strcmpi({fileList.name}, filename));
-    filePath = file.folder;    
+function add_brainstorm3_path()
+    if is_bst3_folder_in_matlab_path()
+        disp('Brainstorm3 folder already added.');
+        return
+    end            
+    bst3_folder = uigetdir(pwd, 'Select brainstorm3 folder');
+    if isequal(bst3_folder, 0)
+        return
+    end
+    [~, folderName] = fileparts(bst3_folder);
+    expected_folder_name = 'brainstorm3';
+    if ~strcmp(folderName, expected_folder_name)
+        error(['Wrong folder (' folderName '). Should be ' expected_folder_name '.']);
+    end
+    if ~isdeployed()
+        addpath(bst3_folder);
+        disp('Brainstorm3 folder added.');
+        brainstorm setpath
+    end
+end
+
+function bool = is_bst3_folder_in_matlab_path()
+    [~, folders_in_search_paths] = fileparts(regexp(path, pathsep, 'Split'));
+    bool = any(strcmp('brainstorm3', folders_in_search_paths)); 
 end
