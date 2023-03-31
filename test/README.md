9cf640aa2dda3b6c2ace4a1abe49a25056b8b8c1
CorentinLabelle <corentinlabelle2@gmail.com>
2023-03-31 14:35:55 -0400

    Add test folder

---
+++ test/README.md
@@ -0,0 +1,5 @@
+# Test
+To execute the test, execute the following command:
+```bash run_test.sh <MATLABROOT>```
+
+For this test, a simplified version of the dataset Face13 (only the subject 1 and 2) is used. A simple EEG pre-processing pipeline is apllied to the data.
---
+++ test/run_test.sh
@@ -0,0 +1,26 @@
+# First argument
+matlab_root=$1
+if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
+    echo "There should be exactly one input which is the path to the Matlab Runtime directory."
+    exit 1
+fi
+
+# Get current file directory
+current_file_dir=$(dirname "$0")
+cd $current_file_dir
+
+# Build paths
+data="${PWD}/data/Face13_S01_S02"
+pipeline="${PWD}/pipeline/eeg_pre_processing.json"
+
+# Move to compile folder
+cd "${PWD}/../compiled_tool"
+
+# Build instruction
+instruction="bash ./run_bst_tool.sh ${matlab_root} ${data} ${pipeline}"
+
+# Display instruction
+echo -e '\n'$instruction'\n'
+
+# Execute
+$instruction
