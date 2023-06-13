9189b9ac815ca157fb1186da8d1c66cbddf239f5
corentin <corentinlabelle2@gmail.com>
2023-06-13 08:21:40 -0400

    Add case for cell

--- domain/utility/sFiles_functions/sFile_to_command.m
+++ domain/utility/sFiles_functions/sFile_to_command.m
@@ -9,6 +9,8 @@ function command = sFile_to_command(sFile)
             command = sFile;                    
         case 'struct'
             command = to_command_from_struct(sFile);
+        case 'cell'
+            command = to_command_from_cell(sFile);
     end
 end
 
@@ -16,6 +18,14 @@ function command = to_command_from_struct(sFile)
     str = strings(1, length(sFile));
     for i = 1:length(sFile)
         str{i} = ['''' sFile(i).FileName ''''];
+    end
+    command = ['{' char(strjoin(str, ', ')) '}'];
+end
+
+function command = to_command_from_cell(sFile)
+    str = string(sFile);
+    for i = 1:length(str)
+        str{i} = ['''' str{i} ''''];
     end
     command = ['{' char(strjoin(str, ', ')) '}'];
 end
