208cd80b7d5a31a5c74e73adc6396f55c606d08b
corentin <corentinlabelle2@gmail.com>
2023-06-26 13:31:49 -0400

    Add "end"

--- domain/utility/protocol_functions/protocol_get_current.m
+++ domain/utility/protocol_functions/protocol_get_current.m
@@ -2,3 +2,4 @@ function [protocol_name, protocol_index] = protocol_get_current()
     protocol_info = bst_get('ProtocolInfo');
     protocol_name = protocol_info.Comment;
     protocol_index = protocol_get_index(protocol_name);
+end
--- get_brainstorm_tool_dir.m
+++ get_brainstorm_tool_dir.m
@@ -1,2 +1,3 @@
 function folder = get_brainstorm_tool_dir()
     folder = fileparts(mfilename('fullpath'));
+end
