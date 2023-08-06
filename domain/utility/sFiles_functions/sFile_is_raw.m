525a9c9dcacd871a09c22adf16f7c65865ea3668
corentin <corentinlabelle2@gmail.com>
2023-08-06 15:07:10 -0400

    Check 'FileType' field of sFile

--- domain/utility/sFiles_functions/sFile_is_raw.m
+++ domain/utility/sFiles_functions/sFile_is_raw.m
@@ -1,7 +1,8 @@
 function isRaw = sFile_is_raw(sFile)            
-    study_file = load(sFile_get_study_path(sFile));
-    isRaw = true;
-    if strcmp(study_file.F.format, 'BST-BIN')
-        isRaw = false;
-    end            
+%     study_file = load(sFile_get_study_path(sFile));
+%     isRaw = true;
+%     if strcmp(study_file.F.format, 'BST-BIN')
+%         isRaw = false;
+%     end      
+    isRaw = strcmpi(sFile.FileType, 'raw');
 end
